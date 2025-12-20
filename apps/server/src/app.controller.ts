49704bba2c976eccd7af1b3ef31f638eafdb79be
gybraty <220001@astanait.edu.kz>
2025-12-20 19:19:54 +0500

    add swagger to backend

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,16 +1,28 @@
 import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { AuthGuard } from './auth/auth.guard';
 import { AppService } from './app.service';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Get hello message' })
+  @ApiResponse({ status: 200, description: 'Return hello message.' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get private data' })
+  @ApiResponse({ status: 200, description: 'Return user data.' })
   @UseGuards(AuthGuard)
   @Get('private')
   getPrivate(@Request() req): any {
