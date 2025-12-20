442efd07f0e869a1422e2197e87ff825b8cf373b
gybraty <220001@astanait.edu.kz>
2025-12-20 15:52:00 +0500

    add routing to frontend

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import { ComponentExample } from "@/components/component-example";
+import LoginPage from "@/login/page";
+import SignupPage from "@/signup/page";
 
 export function App() {
-return <ComponentExample />;
+  return (
+    <Routes>
+      <Route path="/" element={<ComponentExample />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  );
 }
 
 export default App;
