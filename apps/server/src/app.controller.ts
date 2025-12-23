8b08aaf8263985d8f157219e0156262a100e7738
gybraty <220001@astanait.edu.kz>
2025-12-24 03:50:57 +0500

    remove extra endpoint

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,11 +1,5 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
-import {
-  ApiTags,
-  ApiOperation,
-  ApiResponse,
-  ApiBearerAuth,
-} from '@nestjs/swagger';
-import { AuthGuard } from './auth/auth.guard';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @ApiTags('app')
@@ -18,14 +12,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @ApiBearerAuth()
-  @ApiOperation({ summary: 'Get private data' })
-  @ApiResponse({ status: 200, description: 'Return user data.' })
-  @UseGuards(AuthGuard)
-  @Get('private')
-  getPrivate(@Request() req): any {
-    return req.user;
   }
 }
